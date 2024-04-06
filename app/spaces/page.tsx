a9abd4e7f0b06fe4b0820e6e8990f10df0a758fb
Hrishikesh Srihari <rishisrihari9@gmail.com>
2024-04-05 20:35:50 -0700

    stuff

---
+++ app/spaces/page.tsx
@@ -0,0 +1,12 @@
+
+
+export default function GenericPage() {
+    return (
+      <main className="flex items-center justify-center">
+        <div className="z-10 text-sm lg:flex">
+          <span className="text-6xl font-mono">coauthor.ai</span>
+        </div>
+      </main>
+    );
+  }
+  

87c781896913b889657c00ede101f59f6f649342
Abhi Arya <abhigyaa@uci.edu>
2024-04-05 17:57:44 -0700

    fix(aws): add aws url to main.py

--- server/main.py
+++ server/main.py
@@ -1,9 +1,14 @@
 from fastapi import FastAPI
 from mangum import Mangum 
 
+AWS_URL = "https://kcg52nngxu3ihw6e5mmusqdyse0bchdg.lambda-url.us-east-2.on.aws/"
+
 app = FastAPI()
 handler = Mangum(app)
 
 @app.get("/")
 async def say_hi():
-    return {"message": "Hello from CoAuthor API!"}
+    return {
+        "message": "Hello from CoAuthor API!",
+        "url": f"Serving on AWS URL: {AWS_URL}"
+        }
